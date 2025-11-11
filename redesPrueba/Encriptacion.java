e374b02320196cdeac74b87efdebd1913af46417
Teo418 <teoibanham@gmail.com>
2025-11-11 15:12:01 -0300

    subo pero no probamos

--- redesPrueba/Encriptacion.java
+++ redesPrueba/Encriptacion.java
@@ -141,4 +141,12 @@ public class Encriptacion {
         }
         return data;
     }
+    public String bytesToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder(bytes.length * 2);
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+
 }
